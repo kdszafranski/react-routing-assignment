ff052a6ce1aa35c013504ba44be38a26a2ebe21f
Kris Szafranski <kris@thoughtshelter.com>
2018-04-26 13:35:56 -0500

    added about component/page and with multiple components in home Route

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>About this App</h1>
+                <p>Woot! This stuff is the bestest.</p>
+            </div>
+        )
+    }
+}
+
+export default About;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import './App.css';
 import StudentForm from '../StudentForm/StudentForm';
 import StudentList from '../StudentList/StudentList.js'
+import About from '../About/About';
+
+
 class App extends Component {
   constructor() {
     super();
@@ -20,12 +25,12 @@ class App extends Component {
     console.log(newStudent);
     // POST your data here to /students
     axios.post('/students', newStudent)
-    .then((response) => {
-      console.log('response', response);
-      this.getStudents();
-    }).catch( (error) => {
-      console.log(error);
-    });
+      .then((response) => {
+        console.log('response', response);
+        this.getStudents();
+      }).catch((error) => {
+        console.log(error);
+      });
   }
   getStudents() {
     axios.get('/students').then((response) => {
@@ -52,17 +57,36 @@ class App extends Component {
     console.log(this.state);
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">GitHub Student List</h1>
-        </header>
-        <br/>
-        <StudentForm addStudent={this.addStudent}/>
 
-        <p>Student list goes here.</p>
-        <StudentList 
-          studentList={this.state.studentList}
-          handleGetMoreInfo={this.handleGetMoreInfo}/>
-        </div>
+        <Router>
+          <div>
+            <header className="App-header">
+              <nav>
+                <ul><Link to="/">Home</Link></ul>
+                <ul><Link to="/about">About this App</Link></ul>
+              </nav>
+            </header>
+
+            <Route exact path="/" render={props =>
+              <div>
+                <h1 className="App-title">GitHub Student List</h1>
+                <StudentForm addStudent={this.addStudent} />
+
+                <StudentList
+                  studentList={this.state.studentList}
+                  handleGetMoreInfo={this.handleGetMoreInfo} />
+
+              </div>
+            }
+            />
+
+            <Route path="/about" component={About} />
+
+          </div>
+        </Router>
+
+      </div >
+
     );
   }
 }
